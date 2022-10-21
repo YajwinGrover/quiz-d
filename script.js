a8b505fe6e2262c0a3ff7470ec8b4810e3e21276
YajwinGrover <yajwin.grover@gmail.com>
2022-10-21 10:30:43 -0700

    Yes

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.getElementById("body").addEventListener("keydown", function(event){
+//     console.log("DDEZ")
+//     var div = document.createElement('div');
+//     div.className = "card";
+//     document.getElementById("cards").appendChild(div);
+// })

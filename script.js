f6c1513f525423f508af2fa3d17201039ec4f6f9
YajwinGrover <yajwin.grover@gmail.com>
2022-10-29 13:09:29 -0700

    I added create button and animatation

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let key = 'numCards'
-document.onload = produce()
+let key2 = 'ids'
+// localStorage.setItem(key,'')
+// localStorage.setItem(key2,'')
 
 runningCounter = 0
+ids = []
 
 if(localStorage.hasOwnProperty(key)){
     runningCounter = localStorage.getItem(key)
 }
+if(localStorage.hasOwnProperty(key2)){
+    // ids = JSON.parse(localStorage.getItem(key2))
+}
 
-console.log(runningCounter)
-
-document.getElementById("body").addEventListener("keydown", function(event){
-    spawn()
-})
+console.log(ids)
+document.onload = produce()
+// document.getElementById("body").addEventListener("keydown", function(event){
+//     spawn()
+// })
 
 function onClick(ele){
     sessionStorage.setItem('id',ele.id)
     window.location.href = "newIndex.html";
+    
 }
 
 function spawn(){
@@ -24,24 +31,41 @@ function spawn(){
     div.className = "card";
     div.setAttribute('onclick','onClick(this)')
     div.id = Math.floor(Math.random() * 100).toString()
+    ids.push(div.id)
     div.textContent = div.id;
     document.getElementById("cards").appendChild(div);
     runningCounter ++;
     localStorage.setItem(key,runningCounter)
+    localStorage.setItem(key2,JSON.stringify(ids))
 }
 function produce(){
-    counter = localStorage.getItem(key)
-
-    for(let i = 0; i < counter;i++){
+    for(let i = 0; i < runningCounter;i++){
         var div = document.createElement('div');
         div.className = "card";
         div.setAttribute('onclick','onClick(this)')
-        div.id = Math.floor(Math.random() * 100).toString()
+        div.id = ids[i]
         div.textContent = div.id;
         document.getElementById("cards").appendChild(div);
     }
 }
+function hoverCreate(){
+    document.getElementById('create').style.transform = 'translateX(-10px) translateY(-10px)';
+    document.getElementById('border').style.transform = 'translateX(-20px) translateY(-20px)'
+    document.getElementById('plus').style.transform = 'translateX(-20px) translateY(-20px) rotate(90deg)'
+    document.getElementById('plus').style.textShadow = '14px -14px #212631'
 
+    
 
+}
 
+function outHoverCreate(){
+    document.getElementById('create').style.transform = 'translateX(0px) translateY(0px)';
+    document.getElementById('border').style.transform = 'translateX(0px) translateY(0px)'
+    document.getElementById('plus').style.transform = 'translateX(0px) translateY(0px)'
+    document.getElementById('plus').style.textShadow = '7px 7px #212631'
+}
+
+function createPage(){
+    window.location.href = 'creating/createPage.html';
+}
 //TODO: Work on storing things
